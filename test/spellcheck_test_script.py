3c5327e705ecd95b3576862f37ef5dba4acec753
Hannah Walsh <hannah.s.walsh@nasa.gov>
2021-03-10 16:50:47 -0800

    spellcheck test script modified for separate segmentation and spellcheck functions; also prints a sample of words that are corrected

--- test/spellcheck_test_script.py
+++ test/spellcheck_test_script.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 @author: hswalsh
-This script allows you to assess the spellchecker effect.
+This script allows you to assess the spellchecker and segmentation effects.
 """
 import sys
 import os
@@ -17,24 +17,44 @@ num_topics ={'Lesson(s) Learned':5, 'Driving Event':5, 'Recommendation(s)':5}
 
 print('SPELLCHECKER TESTS')
 
-print('Generating topic model object without spellchecker...')
-no_spellcheck = Topic_Model_plus(list_of_attributes=list_of_attributes, document_id_col=document_id_col, csv_file=csv_file_name, name=name, combine_cols=True,LLIS=1,spellcheck=0)
+print('Generating topic model object without spellchecker or segmentation...')
+no_spellcheck = Topic_Model_plus(list_of_attributes=list_of_attributes, document_id_col=document_id_col, csv_file=csv_file_name, name=name, combine_cols=True,quot_correction=True,spellcheck=False,segmentation=False)
 no_spellcheck.prepare_data()
 no_spellcheck.preprocess_data()
 no_spellcheck.hlda()
 
 print('Generating topic model object with spellchecker...')
-spellcheck = Topic_Model_plus(list_of_attributes=list_of_attributes, document_id_col=document_id_col, csv_file=csv_file_name, name=name, combine_cols=True,LLIS=1,spellcheck=1)
+spellcheck = Topic_Model_plus(list_of_attributes=list_of_attributes, document_id_col=document_id_col, csv_file=csv_file_name, name=name, combine_cols=True,quot_correction=True,spellcheck=True,segmentation=False)
 spellcheck.prepare_data()
 spellcheck.preprocess_data()
 spellcheck.hlda()
+
+print('Generating topic model object with segmentation...')
+segmentation = Topic_Model_plus(list_of_attributes=list_of_attributes, document_id_col=document_id_col, csv_file=csv_file_name, name=name, combine_cols=True,quot_correction=True,spellcheck=False,segmentation=True)
+segmentation.prepare_data()
+segmentation.preprocess_data()
+segmentation.hlda()
+
+print('Generating topic model object with both...')
+both = Topic_Model_plus(list_of_attributes=list_of_attributes, document_id_col=document_id_col, csv_file=csv_file_name, name=name, combine_cols=True,quot_correction=True,spellcheck=True,segmentation=True)
+both.prepare_data()
+both.preprocess_data()
+both.hlda()
 
 print('Testing difference in coherence...')
 
 no_spellcheck_coherence = no_spellcheck.hlda_coherence['Combined Text']['average']
 spellcheck_coherence = spellcheck.hlda_coherence['Combined Text']['average']
+segmentation_coherence = segmentation.hlda_coherence['Combined Text']['average']
+both_coherence = both.hlda_coherence['Combined Text']['average']
 
-print('Coherence without spellcheck: ',no_spellcheck_coherence)
-print('Coherence with spellcheck: ',spellcheck_coherence)
+print('Coherence without spellcheck or segmentation: ',no_spellcheck_coherence)
+print('Coherence with spellcheck only: ',spellcheck_coherence)
+print('Coherence with segmentation only: ',segmentation_coherence)
+print('Coherence with spellcheck and segmentation: ',both_coherence)
 
-# prints a sample of corrected words
+print('Sample of spellcheck corrected words...')
+print(spellcheck.correction_list[0:10])
+
+print('Sample of segmentation corrected words...')
+print(segmentation.correction_list[0:10])
