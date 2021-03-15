8c000ff4b094ac87d67f94cebd662a2b4d4fb5f9
Hannah Walsh <hannah.s.walsh@nasa.gov>
2021-03-15 16:09:09 -0700

    added lowercase, lemmatize, and stopword tests

--- test/preprocessing_test_script.py
+++ test/preprocessing_test_script.py
@@ -33,6 +33,21 @@ class test_preprocessing_methods(unittest.TestCase):
         test_segment_text_result = test_class._Topic_Model_plus__segment_text([['devicesthe','nasa','correct']])
         correct_segmentation = [['devices','the','as','a','correct']]
         self.assertEqual(test_segment_text_result,correct_segmentation)
+    def test_lowercase_texts(self):
+        test_class = Topic_Model_plus()
+        test_lowercase_texts_result = test_class._Topic_Model_plus__lowercase_texts([['The','the','THE']])
+        correct_lowercase = [['the','the','the']]
+        self.assertEqual(test_lowercase_texts_result,correct_lowercase)
+    def test_lemmatize_texts(self):
+        test_class = Topic_Model_plus()
+        test_lemmatize_texts_result = test_class._Topic_Model_plus__lemmatize_texts([['start','started','starts','starting']])
+        correct_lemmatize = [['start','start','start','start']]
+        self.assertEqual(test_lemmatize_texts_result,correct_lemmatize)
+    def test_remove_stopwords(self):
+        test_class = Topic_Model_plus()
+        test_remove_stopwords_result = test_class._Topic_Model_plus__remove_stopwords([['system','that','can','be']],domain_stopwords=[])
+        correct_remove_stopwords = [['system']]
+        self.assertEqual(test_remove_stopwords_result,correct_remove_stopwords)
 
 if __name__ == '__main__':
     unittest.main()
