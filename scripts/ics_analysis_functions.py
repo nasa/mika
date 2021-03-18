0dff997cb61feaec42362b605a20e1ae541c5025
srandrade <sequoia.r.andrade@nasa.gov>
2021-03-18 15:16:15 -0700

    update script to work with folder structure

--- scripts/ics_analysis_functions.py
+++ scripts/ics_analysis_functions.py
@@ -35,14 +35,27 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
 from module.topic_model_plus_class import Topic_Model_plus
 from module.stopwords.ICS_stop_words import stop_words
 
+import sys
+import os
+
+from sys import platform
+if platform == "darwin":
+    #sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
+    smart_nlp_path = ''
+elif platform == "win32":
+    sys.path.append('../')
+    smart_nlp_path = os.getcwd()
+    smart_nlp_path = "\\".join([smart_nlp_path.split("\\")[i] for i in range(0,len(smart_nlp_path.split("\\"))-1)]+"/")
+    
+from module.topic_model_plus_class import Topic_Model_plus
 ICS_stop_words = stop_words
 
 list_of_attributes = ["REMARKS", "SIGNIF_EVENTS_SUMMARY", "MAJOR_PROBLEMS"]
 document_id_col = "INCIDENT_ID"
 extra_cols = ["CY","DISCOVERY_DATE", "START_YEAR", "REPORT_DOY", "DISCOVERY_DOY",
               "TOTAL_PERSONNEL", "TOTAL_AERIAL", "PCT_CONTAINED_COMPLETED"]
-file_name = r"C:\Users\srandrad\smart_nlp\input data\209-PLUS\ics209-plus-wildfire\ics209-plus-wildfire\ics209-plus-wf_sitreps_1999to2014.csv"
-name = r"\output data\ICS_full"
+file_name = smart_nlp_path+"\input data\209-PLUS\ics209-plus-wildfire\ics209-plus-wildfire\ics209-plus-wf_sitreps_1999to2014.csv"
+name = smart_nlp_path+r"\output data\ICS_full"
 #list_of_attributes = ["Combined Text"]
 
 #ICS = Topic_Model_plus(document_id_col=document_id_col, extra_cols=extra_cols, csv_file=file_name, list_of_attributes=list_of_attributes, name=name, combine_cols=True)
@@ -58,12 +71,12 @@ name = r"\output data\ICS_full"
 
 r"""
 
-file = r"C:\Users\srandrad\smart_nlp\output data\ICS_fullset_combinedtopics-Mar-05-2021\preprocessed_data.csv"
+file = smart_nlp_path+r"\output data\ICS_fullset_combinedtopics-Mar-05-2021\preprocessed_data.csv"
 ICS = Topic_Model_plus(document_id_col=document_id_col, extra_cols=extra_cols, list_of_attributes=list_of_attributes, name=name, combine_cols=True)
 ICS.extract_preprocessed_data(file)
 ICS.ngrams = "custom"
 ICS.data_df = ICS.data_df[:5000].reset_index(drop=True)
-#ICS.extract_preprocessed_data(r"C:\Users\srandrad\smart_stereo\ICS_TEST_full_topics-Feb-25-2021-\preprocessed_data.csv")
+#ICS.extract_preprocessed_data(smart_nlp_path+r"\output data\ICS_TEST_full_topics-Feb-25-2021-\preprocessed_data.csv")
 
 #ICS.lda_optimization(min_cf=5, max_topics = 200)
 num_topics = {attr:40 for attr in list_of_attributes}
@@ -79,13 +92,16 @@ ICS.save_hlda_coherence()
 ICS.save_hlda_taxonomy()
 ICS.save_hlda_level_n_taxonomy()
 """
-"""Analyze hazard trends for each hazard:
+"""Analyze hazard trends for each hazard: see notebook
     -for each document: 
         -find its most representative topics. for each word, if it is in the topic,
-        identify MTTF, store in dictionary with years as keys and lists of MTTF as values. 
-        Two versions, one in raw time, one in percent containment
+        identify time of operational occurence, store in dictionary with years as keys 
+        and lists of times as values. 
+        **Two versions, one in raw time, one in percent containment
         - go through the data set, if word present, add +1 to frequency dictionary. Frequency
         dictionary has years as keys, frequency as value. Monthly frequecy has month-year as keys,
-        frequency as value.
-    - what time during operation it occurs (MTTF): report doy - discovery doy
-    - frequency -> use this to derive how often it occurs (MTBF)"""
+        frequency as value. Also add the fire id to a dictionary with keys as hazards, values as lists of fire ids.
+        -find fire rate by doing total fire ids/ hazard fire ids
+        -find annual rate by dividing total frequency/ total years
+    - what time during operation it occurs: report doy - discovery doy
+    - frequency -> use this to derive how often it occurs """
