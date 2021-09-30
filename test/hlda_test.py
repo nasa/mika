00b952614b2a2ec3854c8ca2463c79158247a1c2
hswalsh <hannah.s.walsh@nasa.gov>
2021-09-30 11:59:58 -0700

    small progress on cross-platform compatibility for main module and tests

--- test/hlda_test.py
+++ test/hlda_test.py
@@ -5,24 +5,11 @@ Created on Thu Mar 18 17:33:36 2021
 @author: srandrad
 """
 
-
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
-from sys import platform
-if platform == "darwin":
-    sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
-    smart_nlp_path = ''
-elif platform == "win32":
-    sys.path.append('../')
-    smart_nlp_path = os.getcwd()
-    smart_nlp_path = "\\".join([smart_nlp_path.split("\\")[i] for i in range(0,len(smart_nlp_path.split("\\"))-1)])
-    
-import unittest
-
 from module.topic_model_plus_class import Topic_Model_plus
 import pandas as pd
 test_preprocessed_data = pd.DataFrame()
+
+import unittest
 
 def truncate(num, digits):
    sp = str(num).split('.')
@@ -51,7 +38,7 @@ class test_hlda_methods(unittest.TestCase):
     def test_hlda_functions(self): # integration test
         list_of_attributes = ['Lesson(s) Learned','Driving Event','Recommendation(s)']
         document_id_col = 'Lesson ID'
-        csv_file_name = smart_nlp_path+r"\input data\preprocessed_data.csv"
+        csv_file_name = os.path.join('..','data','preprocessed_data_LLIS.csv')
         test_hlda = Topic_Model_plus(list_of_attributes=list_of_attributes, document_id_col=document_id_col)
         test_hlda.extract_preprocessed_data(csv_file_name)
         test_hlda.hlda(training_iterations=100)
@@ -63,13 +50,13 @@ class test_hlda_methods(unittest.TestCase):
         test_hlda.save_hlda_taxonomy()
         file_path = test_hlda.folder_path
         
-        doc_topics1 = pd.read_csv(file_path+"/hlda_topic_dist_per_doc.csv")#.applymap(str)
-        tax1 = pd.read_csv(file_path+"/hlda_taxonomy.csv").applymap(str)
-        coherence_1 = pd.read_csv(file_path+"/"+"hlda_coherence.csv").applymap(str)
-        level_1_tax1 = pd.read_csv(file_path+"/hlda_level1_taxonomy.csv").applymap(str)
+        doc_topics1 = pd.read_csv(os.path.join(file_path,"hlda_topic_dist_per_doc.csv"))#.applymap(str)
+        tax1 = pd.read_csv(os.path.join(file_path,"hlda_taxonomy.csv")).applymap(str)
+        coherence_1 = pd.read_csv(os.path.join(file_path,"hlda_coherence.csv")).applymap(str)
+        level_1_tax1 = pd.read_csv(os.path.join(file_path,"hlda_level1_taxonomy.csv")).applymap(str)
         topics1 = {}
         for attr in list_of_attributes:
-            topics1[attr] = pd.read_csv(file_path+"/"+attr+"_hlda_topics.csv").applymap(str)
+            topics1[attr] = pd.read_csv(os.path.join(file_path,attr+"_hlda_topics.csv")).applymap(str)
         
         #testing functions from bin
         file_path = test_hlda.folder_path 
@@ -80,20 +67,20 @@ class test_hlda_methods(unittest.TestCase):
         test_hlda.save_hlda_topics()
         test_hlda.save_hlda_level_n_taxonomy()
         
-        doc_topics2 = pd.read_csv(file_path+"/hlda_topic_dist_per_doc.csv")#.applymap(str)
-        tax2 = pd.read_csv(file_path+"/hlda_taxonomy.csv").applymap(str)
-        coherence_2 = pd.read_csv(file_path+"/"+"hlda_coherence.csv").applymap(str)
-        level_1_tax2 = pd.read_csv(file_path+"/hlda_level1_taxonomy.csv").applymap(str)
+        doc_topics2 = pd.read_csv(os.path.join(file_path,"hlda_topic_dist_per_doc.csv"))#.applymap(str)
+        tax2 = pd.read_csv(os.path.join(file_path,"hlda_taxonomy.csv")).applymap(str)
+        coherence_2 = pd.read_csv(os.path.join(file_path,"hlda_coherence.csv")).applymap(str)
+        level_1_tax2 = pd.read_csv(os.path.join(file_path,"hlda_level1_taxonomy.csv")).applymap(str)
         topics2 = {}
         for attr in list_of_attributes:
-            topics2[attr] = pd.read_csv(file_path+"/"+attr+"_hlda_topics.csv").applymap(str)
+            topics2[attr] = pd.read_csv(os.path.join(file_path,attr+"_hlda_topics.csv")).applymap(str)
         
         #delete test folder/everything in it
         for root, dirs, files in os.walk(file_path):
             for file in files:
                 os.remove(os.path.join(root, file))
         os.rmdir(file_path)
-        os.rmdir(os.getcwd()+"/output data")
+        os.rmdir(os.path.join(os.getcwd(),"results"))
         
         
         #rounding to account for differences due to float number system
