eea18751f33ebc8201a18178fb3c2bee879c43a4
srandrade <sequoia.r.andrade@nasa.gov>
2021-12-13 18:04:25 -0800

    added in time risk matrix functionality for word2vec and bert models

--- in_time_risk_matrix_script.py
+++ in_time_risk_matrix_script.py
@@ -37,15 +37,43 @@ meta_inputs = ["TOTAL_PERSONNEL", "TOTAL_AERIAL", "PCT_CONTAINED_COMPLETED",
               'Incident_region_SWCC', 'INC_MGMT_ORG_ABBREV_1', 'INC_MGMT_ORG_ABBREV_2','INC_MGMT_ORG_ABBREV_3', 
               'INC_MGMT_ORG_ABBREV_4','INC_MGMT_ORG_ABBREV_5', 'INC_MGMT_ORG_ABBREV_B','INC_MGMT_ORG_ABBREV_C', 
               'INC_MGMT_ORG_ABBREV_D','INC_MGMT_ORG_ABBREV_E', 'INC_MGMT_ORG_ABBREV_F']
-
+"""
+#tfidf_model
 text_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','likelihood_model_1_test.sav')
 meta_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','likelihood_model_2_test.sav')
 rm.load_model(model_location=[text_model, meta_model], model_type="likelihood", model_inputs=[text_input, meta_inputs])
 #vectorized
 tfidf_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','likelihood_tfidf_model_1_test.sav')
 rm.load_nlp_model(model_location=[tfidf_model], model_type="likelihood", model_number=[0], model_input=[["Combined_Text"]])
-#input_reports = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()),'data',"likelihood_model_input_test.csv"))
-#input_reports = input_reports.iloc[50:52][:].drop(["Unnamed: 0"], axis=1).reset_index(drop=True)
 input_reports = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','data','ICS_predictive_sitreps_test.csv')).drop(["Unnamed: 0"], axis=1)
-input_reports = input_reports.iloc[50:52][:].reset_index(drop=True)
+input_reports = input_reports.iloc[40:42][:].reset_index(drop=True)
 rm.build_risk_matrix(input_reports, clean=True, vectorize=True, target="Combined_Text", model_type="likelihood", nlp_model_type="tfidf", nlp_model_number=0)
+"""
+#word2vec_model
+"""
+text_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','word2vec_LSTM_NN')
+meta_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','likelihood_model_word2vec_2_test.sav')
+rm.load_model(model_location=[text_model, meta_model], model_type="likelihood", NN=True, model_inputs=[text_input, meta_inputs])
+
+bigram_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','word2vec_bigram_model.sav')
+trigram_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','word2vec_trigram_model.sav')
+vectorization_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','word2vec_tokenizer.sav')
+model_numbers = ["bigram_0", "trigram_0", "tokenizer_0"]
+rm.load_nlp_model(model_location=[bigram_model, trigram_model, vectorization_model], model_type="likelihood", 
+                  model_number=model_numbers, model_input=[["Combined_Text"],["Combined_Text"],["Combined_Text"]])
+input_reports = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','data','ICS_predictive_sitreps_test.csv')).drop(["Unnamed: 0"], axis=1)
+input_reports = input_reports.iloc[40:42][:].reset_index(drop=True)
+rm.build_risk_matrix(input_reports, clean=True, vectorize=True, target="Combined_Text", model_type="likelihood", nlp_model_type="word2vec", nlp_model_number=0)
+"""
+#BERT model
+#"""
+text_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','sbert_likelihood_model1.sav')
+meta_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','sbert_likelihood_model2.sav')
+rm.load_model(model_location=[text_model, meta_model], model_type="likelihood", model_inputs=[text_input, meta_inputs])
+#vectorized
+bert_model = os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','models','sbert_model.sav')
+rm.load_nlp_model(model_location=[bert_model], model_type="likelihood", model_number=[0], model_input=[["Combined_Text"]])
+input_reports = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()),'smart_nlp','data','ICS_predictive_sitreps_test.csv')).drop(["Unnamed: 0"], axis=1)
+input_reports = input_reports.iloc[40:42][:].reset_index(drop=True)
+rm.build_risk_matrix(input_reports, clean=True, vectorize=True, target="Combined_Text", model_type="likelihood", nlp_model_type="sBERT", nlp_model_number=0)
+#"""
--- module/In_time_risk_matrix_class.py
+++ module/In_time_risk_matrix_class.py
@@ -11,13 +11,16 @@ import pandas as pd
 import seaborn as sn
 import numpy as np
 from tensorflow.keras import preprocessing as kprocessing
+from tensorflow.keras import models
+from sklearn.metrics import hamming_loss
 
 class In_Time_Risk_Matrix():
     def __init__(self, num_severity_models=1, num_likelihood_models=1, hazards=[]):
         self.severity_model_count = num_severity_models
         self.likelihood_model_count = num_likelihood_models
         self.hazards = hazards
-    def load_model(self,model_location=[], model_type=None, model_inputs=[]):
+        self.NN = False
+    def load_model(self,model_location=[], model_type=None, model_inputs=[], NN=False):
         """
         Loads models and saves as object attributes
 
@@ -36,9 +39,19 @@ class In_Time_Risk_Matrix():
 
         """
         if model_location and model_type:
-            for i in range(len(model_location)):
-                setattr(self, str(model_type)+'_model_'+str(i), pickle.load(open(model_location[i], 'rb')))
-                setattr(self, str(model_type)+'_model_inputs_'+str(i), model_inputs[i])
+            if not NN :
+                for i in range(len(model_location)):
+                    setattr(self, str(model_type)+'_model_'+str(i), pickle.load(open(model_location[i], 'rb')))
+                    setattr(self, str(model_type)+'_model_inputs_'+str(i), model_inputs[i])
+            else:
+                self.NN = True
+                for i in range(len(model_location)):
+                    if ".sav" in model_location[i]:
+                        setattr(self, str(model_type)+'_model_'+str(i), pickle.load(open(model_location[i], 'rb')))
+                        setattr(self, str(model_type)+'_model_inputs_'+str(i), model_inputs[i])
+                    else:
+                        setattr(self, str(model_type)+'_model_'+str(i), models.load_model(model_location[i], custom_objects={'Hamming_loss':hamming_loss}))
+                        setattr(self, str(model_type)+'_model_inputs_'+str(i), model_inputs[i])
     
     def load_nlp_model(self, model_location=[], model_type=None, model_number=[], model_input=[]):
         """
@@ -87,13 +100,13 @@ class In_Time_Risk_Matrix():
             vect_df =  vectorizer.transform(input_df[target]).todense()
             new_cols = vectorizer.get_feature_names()
             vect_df = pd.DataFrame(vect_df, columns=new_cols)
-            #print(new_cols)
             setattr(self, 'likelihood_model_inputs_'+str(nlp_model_number), new_cols)
+            
         elif nlp_model_type == 'word2vec':
             #saves bigram and trigram dectector and tokenizer
-            bigrams_detector = getattr(self, str(model_type)+'_nlp_model_bigram'+str(nlp_model_number))
-            trigrams_detector = getattr(self, str(model_type)+'_nlp_model_trigram'+str(nlp_model_number))
-            tokenizer = getattr(self, str(model_type)+'_nlp_model_tokenizer'+str(nlp_model_number))
+            bigrams_detector = getattr(self, str(model_type)+'_nlp_model_bigram_'+str(nlp_model_number))
+            trigrams_detector = getattr(self, str(model_type)+'_nlp_model_trigram_'+str(nlp_model_number))
+            tokenizer = getattr(self, str(model_type)+'_nlp_model_tokenizer_'+str(nlp_model_number))
             ## create list of n-grams
             lst_corpus = input_df[target]
             ## detect common bigrams and trigrams using the fitted detectors
@@ -104,24 +117,24 @@ class In_Time_Risk_Matrix():
             ## padding sequence
             vect_df = kprocessing.sequence.pad_sequences(lst_text2seq, maxlen=30,
                          padding="post", truncating="post")
-        #print(vect_df.shape)
+        elif nlp_model_type == 'sBERT':
+            vectorizer = getattr(self, str(model_type)+'_nlp_model_'+str(nlp_model_number))
+            vect_df =  vectorizer.encode(input_df[target])
+            self.bert=True
     
         return vect_df
     
     def predict_likelihoods(self, report_df):
-        #complicated - depends on the models used
-        #self.vectorize_input()#TODO: implement this properly -> where does this go in the work flow?? -> in likelihood predictions
         for i in range(self.likelihood_model_count):
             mdl = getattr(self, 'likelihood_model_'+str(i))
             if i == 0:
-                #input_data = [l.reshape(-1, 1) for l in report_df[getattr(self, 'likelihood_model_inputs_'+str(i))].tolist()]
-                #print(getattr(self, 'likelihood_model_inputs_'+str(i)))
-                #print(report_df[getattr(self, 'likelihood_model_inputs_'+str(i))])
-                probs = mdl.predict_proba(report_df[getattr(self, 'likelihood_model_inputs_'+str(i))].values.reshape(1,-1))
+                if self.NN or self.bert:
+                    probs = mdl.predict(report_df[getattr(self, 'likelihood_model_inputs_'+str(i))].values[0].reshape(1,-1))
+                else: 
+                    probs = mdl.predict_proba(report_df[getattr(self, 'likelihood_model_inputs_'+str(i))].values.reshape(1,-1))
                 probs_df = pd.Series(probs[0], index=self.hazards)
             if i>0:
                 input_df = pd.concat([report_df[getattr(self, 'likelihood_model_inputs_'+str(i))], probs_df])
-                #print(input_df)
                 probs = mdl.predict_proba(input_df.values.reshape(1,-1))
                 probs_df = pd.DataFrame(probs, columns=self.hazards)
         return probs_df
@@ -133,7 +146,6 @@ class In_Time_Risk_Matrix():
                    'Livestock', 'Law_Violations', 'Military_Base', 'Infrastructure',
                    'Extreme_Weather', 'Ecological', 'Hazardous_Terrain', 'Floods','Dry_Weather']
         severities = {hazard:[] for hazard in self.hazards}
-        #for i in input_df:
         for hazard in self.hazards:
             temp_input = report_df
             temp_input.at[hazard] = 1
@@ -147,7 +159,7 @@ class In_Time_Risk_Matrix():
         return preds_df
     
     def get_likelihoods(self, report_df):
-        self.curr_likelihoods = {hazard:0 for hazard in self.hazards} #hazard: (likelihood, severity)
+        self.curr_likelihoods = {hazard:0 for hazard in self.hazards}
         probs_df = self.predict_likelihoods(report_df)
         for hazard in self.hazards:
             p = probs_df.at[0, hazard]
@@ -192,26 +204,30 @@ class In_Time_Risk_Matrix():
             vect_df = self.vectorize_inputs(input_reports, target, model_type, nlp_model_type, nlp_model_number)
             if nlp_model_type == "tfidf":
                 input_reports = pd.concat([input_reports, vect_df], axis=1)
-            #target, model_type, nlp_model_type, nlp_model_number
+            elif nlp_model_type == "word2vec" or nlp_model_type == 'sBERT':
+                input_reports[target] = [np.asarray(a).astype('float32') for a in vect_df]
+            
         if condense_text: 
-            condense_dict = {self.hazards[i]:"H"+str(i) for i in range(len(self.hazards))}
+            condense_dict = {self.hazards[i]:"H"+str(i+1) for i in range(len(self.hazards))}
+            
         for i in range(len(input_reports)):
             report = input_reports.iloc[i][:]
             
             annotation_df = pd.DataFrame([["" for i in range(5)] for j in range(5)],
                              columns=['Minimal Impact', 'Minor Impact', 'Major Impact', 'Significant Critical Impact', 'Catastrophic Impact'],
                               index=['Highly Likely', 'Likely', 'Possible','Improbable', 'Extremely Improbable'])
-            #print(report, type(report))
             self.get_likelihoods(report)
             self.get_severities(report)
-            hazards_per_row=0
+            hazards_per_row_df = pd.DataFrame([[0 for i in range(5)] for j in range(5)],
+                             columns=['Minimal Impact', 'Minor Impact', 'Major Impact', 'Significant Critical Impact', 'Catastrophic Impact'],
+                              index=['Highly Likely', 'Likely', 'Possible','Improbable', 'Extremely Improbable'])
             for hazard in self.hazards:
                 if annotation_df.at[self.curr_likelihoods[hazard],self.curr_severities[hazard]]!= "":
                     annotation_df.at[self.curr_likelihoods[hazard],self.curr_severities[hazard]] += ", "
-                    hazards_per_row+=1
-                    if hazards_per_row>2:
+                    hazards_per_row_df.at[self.curr_likelihoods[hazard],self.curr_severities[hazard]]+=1
+                    if hazards_per_row_df.at[self.curr_likelihoods[hazard],self.curr_severities[hazard]]>2:
                         annotation_df.at[self.curr_likelihoods[hazard],self.curr_severities[hazard]]+="\n"
-                        hazards_per_row=1
+                        hazards_per_row_df.at[self.curr_likelihoods[hazard],self.curr_severities[hazard]]=0
                 if condense_text: hazard_annot = condense_dict[hazard]
                 else: hazard_annot = hazard
                 annotation_df.at[self.curr_likelihoods[hazard],self.curr_severities[hazard]] += (str(hazard_annot))
@@ -231,8 +247,8 @@ class In_Time_Risk_Matrix():
             plt.ylabel("Likelihood", fontsize=16)
             plt.xlabel("Severity", fontsize=16)
             minor_ticks = np.arange(1, 6, 1)
-            ax.set_xticks(minor_ticks, minor=True)#, tick_params={'length':0})
-            ax.set_yticks(minor_ticks, minor=True)#, length=0)
+            ax.set_xticks(minor_ticks, minor=True)
+            ax.set_yticks(minor_ticks, minor=True)
             ax.tick_params(which='minor',length=0, grid_color='black', grid_alpha=1)
             ax.grid(which='minor', alpha=1)
             plt.show()
