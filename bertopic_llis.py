800963017321cec94385fa615317d6ff7858635d
hswalsh <hannah.s.walsh@nasa.gov>
2021-12-21 14:56:53 -0800

    added files for word/topic intrusion analysis and beginnings of semantic search and betopic features. added correct references for paper results and data used. began separation of features into different files in module folder - won't break dependencies yet

---
+++ bertopic_llis.py
@@ -0,0 +1,59 @@
+"""
+@author: hswalsh
+"""
+
+import os
+import pandas as pd
+import sys
+from bertopic import BERTopic
+from sentence_transformers import SentenceTransformer
+from umap import UMAP
+from module.calc_coherence import calc_coherence
+from sklearn.feature_extraction.text import CountVectorizer
+import time
+
+# load data - no preprocessing required for bertopic!
+# import LLIS docs
+llis_filename = os.path.join('data','lessons_learned_2021-12-10.xlsx')
+llis_df = pd.read_excel(llis_filename)
+
+# treat paragraphs in llis as separate documents - paragraphs are more "requirement-sized"
+llis_abstract = llis_df['Abstract'].to_list()
+llis_lessons_learned = llis_df['Lesson(s) Learned'].to_list()
+llis_recommendations = llis_df['Recommendation(s)'].to_list()
+llis_driving_event = llis_df['Driving Event'].to_list()
+llis_evidence = llis_df['Evidence'].to_list()
+llis_corpus = llis_abstract + llis_lessons_learned + llis_recommendations + llis_driving_event + llis_evidence
+
+# remove nans
+def remove_nans(docs):
+    for i in range(0,len(docs)):
+        if pd.isnull(docs[i]):
+            docs[i] = ''
+    return docs
+llis_corpus = remove_nans(llis_corpus)
+llis_corpus = list(filter(('').__ne__, llis_corpus))
+
+start_time = time.time() # start timer
+
+# setup embeddings
+sentence_model = SentenceTransformer('all-MiniLM-L6-v2')
+embeddings = sentence_model.encode(llis_corpus, show_progress_bar=False)
+
+# create and train topic model
+umap_model = UMAP(random_state=1) # setting the random state sacrifices performance so that results are consistent between runs
+vectorizer_model = CountVectorizer(ngram_range=(1,3), stop_words="english") # remove stopwords, otherwise topics seem to contain low-information words
+topic_model = BERTopic(umap_model=umap_model, vectorizer_model = vectorizer_model, verbose=True)
+topics, probs = topic_model.fit_transform(llis_corpus, embeddings)
+coherence = calc_coherence(llis_corpus, topics, topic_model, 'c_v')
+
+run_time = time.time() - start_time # end timer
+
+topic_info = topic_model.get_topic_info()
+topic_info['coherence'] = ['coherence'] + coherence
+topic_info.to_csv(os.path.join('results','bertopics_llis.csv'))
+
+if run_time < 60:
+    print("--- %s seconds ---" % (run_time))
+else:
+    print("--- %s minutes ---" % (run_time/60))
---
+++ llis_hls_query.py
@@ -0,0 +1,88 @@
+"""
+@author: hswalsh
+"""
+
+import os
+import pandas as pd
+from sentence_transformers import SentenceTransformer, util
+import torch
+import time
+import csv
+
+# load data
+llis_filename = os.path.join('data','lessons_learned_2021-12-10.xlsx')
+llis_df = pd.read_excel(llis_filename)
+requirements_filename = os.path.join('data','functional_performance_requirements_docs.csv')
+requirements_df = pd.read_csv(requirements_filename)
+
+# treat paragraphs in llis as separate documents - paragraphs are more "requirement-sized"
+llis_abstract = llis_df['Abstract'].to_list()
+llis_lessons_learned = llis_df['Lesson(s) Learned'].to_list()
+llis_recommendations = llis_df['Recommendation(s)'].to_list()
+llis_driving_event = llis_df['Driving Event'].to_list()
+llis_evidence = llis_df['Evidence'].to_list()
+llis_corpus = llis_abstract + llis_lessons_learned + llis_recommendations + llis_driving_event + llis_evidence
+
+# grab relevant metadata
+llis_title = llis_df['Title'].to_list()
+llis_title = llis_title + llis_title + llis_title + llis_title + llis_title
+llis_phase = llis_df['From what phase of the program or project was this lesson learned captured?'].to_list()
+llis_phase = llis_phase + llis_phase + llis_phase + llis_phase + llis_phase
+llis_topics = llis_df['Topics'].to_list()
+llis_topics = llis_topics + llis_topics + llis_topics + llis_topics + llis_topics
+
+# get list of lesson id's aligned with corpus
+llis_numbers = llis_df['Lesson ID'].to_list()
+llis_numbers_corpus = llis_numbers + llis_numbers + llis_numbers + llis_numbers + llis_numbers
+
+# get section names aligned with corpus
+section_names = ['Abstract', 'Lesson(s) Learned', 'Recommendation(s)', 'Driving Event', 'Evidence']
+sections = [llis_abstract, llis_lessons_learned, llis_recommendations, llis_driving_event, llis_evidence]
+llis_sections = []
+for i in range(0,len(sections)):
+    section = sections[i]
+    for j in range(0,len(section)):
+        llis_sections.append(section_names[i])
+        
+# remove nans
+def remove_nans(docs):
+    for i in range(0,len(docs)):
+        if pd.isnull(docs[i]):
+            docs[i] = ''
+    return docs
+llis_corpus = remove_nans(llis_corpus)
+
+# select query
+query = requirements_df['Requirement'][1]
+
+start_time = time.time() # start timer
+
+# get query embedding
+sbert_model = SentenceTransformer('all-MiniLM-L6-v2')
+query_embedding = sbert_model.encode(query, convert_to_tensor=True)
+
+# get corpus embeddings - eventually these should be pre-run and loaded for use in each query
+corpus_embeddings = sbert_model.encode(llis_corpus, convert_to_tensor=True)
+
+# semantic search
+hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
+hits = hits[0] # Get the hits for the first query
+run_time = time.time() - start_time # end timer
+
+# save outputs
+writer = csv.writer(open(os.path.join('results','llis_hls_query_results_1.csv'), "w"))
+writer.writerow(['Query: ',query])
+writer.writerow(['--------------------------------------'])
+for hit in hits:
+    writer.writerow(['Lesson ID: ', str(llis_numbers_corpus[hit['corpus_id']])])
+    writer.writerow([' Title: ', str(llis_title[hit['corpus_id']])])
+    writer.writerow([' LLIS-Designated Topics: ', str(llis_topics[hit['corpus_id']])])
+    writer.writerow([' Phase: ', str(llis_phase[hit['corpus_id']])])
+    writer.writerow([' Section: ', str(llis_sections[hit['corpus_id']])])
+    writer.writerow(["Score: {:.4f}".format(hit['score'])])
+    writer.writerow([llis_corpus[hit['corpus_id']]])
+
+if run_time < 60:
+    print("--- %s seconds ---" % (run_time))
+else:
+    print("--- %s minutes ---" % (run_time/60))
