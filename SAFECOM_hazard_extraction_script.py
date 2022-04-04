749f747f516f984bfd7d56180c5636c680f065fc
srandrade <sequoia.r.andrade@nasa.gov>
2022-04-04 16:06:57 -0700

    updated script with bug fix, bertopic functioinality additions, and hdp option

--- SAFECOM_hazard_extraction_script.py
+++ SAFECOM_hazard_extraction_script.py
@@ -6,7 +6,9 @@ Created on Tue Sep 21 13:19:22 2021
 """
 import pandas as pd
 import os
-
+from hdbscan import HDBSCAN
+from sklearn.feature_extraction.text import CountVectorizer
+import tomotopy as tp
 from module.topic_model_plus_class import Topic_Model_plus
 
 
@@ -29,11 +31,10 @@ extra_cols = ['Agency', 'Region', 'Location', 'Date', 'Date Submitted', 'Trackin
 document_id_col = 'Tracking #'#id'
 csv_file_name = os.path.join('data','SAFECOM_data.csv')
 name = os.path.join('safecom')
-#"""
+"""
 safecom = Topic_Model_plus(list_of_attributes=list_of_attributes, document_id_col=document_id_col, 
                         csv_file=csv_file_name, database_name=name, extra_cols=extra_cols)
 num_topics ={'Narrative': 96}
-#"""
 safecom.prepare_data()
 fire_missions = [mission for mission in list(safecom.data_df['Mission Type']) if type(mission) is str and 'fire' in mission.lower()]
 safecom.data_df = safecom.data_df.loc[safecom.data_df['Mission Type'].isin(fire_missions)].reset_index(drop=True)
@@ -41,14 +42,28 @@ safecom.doc_ids = safecom.data_df[document_id_col].tolist()
 raw_text = safecom.data_df[safecom.list_of_attributes] 
 raw_attrs = ['Raw_'+attr for attr in safecom.list_of_attributes]
 safecom.data_df[raw_attrs] = raw_text
-safecom.preprocess_data()
-safecom.save_preprocessed_data()
+"""
+#safecom.preprocess_data()
+#safecom.save_preprocessed_data()
 #"""
-"""#Extract preprocessed data
-file = os.path.join('results','safecom_topics-Feb-17-2022','preprocessed_data.csv')
+#"""#Extract preprocessed data
+file = os.path.join(os.getcwd(),'results','safecom_topics_Feb-17-2022','preprocessed_data.csv')
 safecom = Topic_Model_plus(document_id_col=document_id_col, extra_cols=extra_cols, list_of_attributes=list_of_attributes, database_name=name, combine_cols=False)
 safecom.extract_preprocessed_data(file)
+#"""
+"""#run hdp to get topic numbers
+safecom.database_name = "SAFECOM_hazards_hdp"
+safecom.hdp()
+for attr in list_of_attributes:
+    print(safecom.hdp_models[attr].k)
+safecom.save_lda_results()
+safecom.save_lda_models()
+for attr in list_of_attributes:
+    safecom.lda_visual(attr)
 """
+#"""
+num_topics ={'Narrative': 100}
+safecom.database_name = "SAFECOM_hazards_lda"
 safecom.lda(min_cf=1, num_topics=num_topics)
 safecom.save_lda_results()
 safecom.save_lda_models()
@@ -56,20 +71,19 @@ for attr in list_of_attributes:
     safecom.lda_visual(attr)
 #"""
 
-
-
-
-#"""#Run hlda
+"""#Run hlda
 safecom.hlda(levels=3, eta=0.50, min_cf=1, min_df=1)
 safecom.save_hlda_models()
 safecom.save_hlda_results()
 for attr in list_of_attributes:
     safecom.hlda_visual(attr)
-#"""
+"""
 
-#Run Bertopic
+"""#Run Bertopic
 safecom.data_df[safecom.list_of_attributes] = raw_text
-safecom.bert_topic()
+vectorizer_model = CountVectorizer(ngram_range=(1, 2), stop_words="english") #removes stopwords
+hdbscan_model = HDBSCAN(min_cluster_size=3, min_samples=3) #allows for smaller topic sizes/prevents docs with no topics
+safecom.bert_topic(count_vectorizor=vectorizer_model, hdbscan=hdbscan_model)
 safecom.save_bert_results()
 #get coherence
 #coh = ICS.get_bert_coherence(coh_method='c_v')
@@ -77,7 +91,4 @@ safecom.save_bert_vis()
 safecom.reduce_bert_topics(num=100)
 safecom.save_bert_results()
 safecom.save_bert_vis()
-
-#filepath = smart_nlp_path+r"/output data/test_safecom_topics-Sep-21-2021"
-#safecom.hlda_extract_models(filepath)
-#safecom.save_hlda_results()
+"""
