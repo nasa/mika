b5d40a65c9fd8128d7a754cd2c6913c090fef63d
srandrade <sequoia.r.andrade@nasa.gov>
2022-08-01 16:35:11 -0700

    updated lexicon generation and risk factor score scripts

--- lexicon_generation_script.py
+++ lexicon_generation_script.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun 15 16:47:08 2022
-
+Updates: 
+    - annotated only text gives the V1 results - chi-squared is calculated 
+    from only risk factor annotated text. This gives words unique to each risk factor.
+    - total text should give different results - chi-squared is caluclated from
+    the total text (both annotated and unannotated). This should still give unique words.
+    Note that this may decrease or increase words - not sure which
+    - pval can be varied by changing pval variable. Default is 0.99, should try 0.95 and 0.9
+    these values should result in more words per risk factor
 @author: srandrad
 """
 
@@ -14,67 +21,107 @@ import os
 import numpy as np
 
 #reading in annotation file
-file = r"C:\Users\srandrad\Downloads\llis_risk_factors.jsonl"
+file = r"C:\Users\srandrad\Downloads\LLIS_150_no_extra.jsonl"
 df = read_doccano_annots(file,  encoding=True) #encoding=True is required when Sequoia is importing data from Hannah
+
+def format_y_labels(y_raw):
+    y = pd.DataFrame(np.zeros((len(y_raw), len(set(y_raw)))), columns=set(y_raw)).sort_index(axis=1)
+    for i in range(len(y_raw)):
+        y.at[i, y_raw[i]] = 1
+    return y
+
+def vectorize_text(X):
+    vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1,3), norm='l2', max_features=10000)
+    vectorizer.fit(X)
+    X_vec = vectorizer.transform(X)
+    return X_vec, vectorizer
+
+def get_significant_words(vectorizer, X_vec, y, p_value_limit=0.99):
+    X_names = vectorizer.get_feature_names_out()
+    p_value_limit = 0.99 #Change this to vary p-value, try 0.95 and 0.9
+    dtf_features = pd.DataFrame()
+    for cat in y.columns:
+        chi2, p = feature_selection.chi2(X_vec, y[[cat]])
+        dtf_features = dtf_features.append(pd.DataFrame(
+                       {"feature":X_names, "score":1-p, "y":cat}))
+        dtf_features = dtf_features.sort_values(["y","score"], 
+                        ascending=[True,False])
+        dtf_features = dtf_features[dtf_features["score"]>p_value_limit]
+    X_names = dtf_features["feature"].unique().tolist() #unnecessary?
+    return dtf_features
+
+def print_results(y, dtf_features):
+    #print top words
+    print("Using only annotated text")
+    for cat in y:
+        print("# {}:".format(cat))
+        print("  . selected features:",
+              len(dtf_features[dtf_features["y"]==cat]))
+        print("  . top features:", ",".join(dtf_features[dtf_features["y"]==cat]["feature"].values[:10]))
+        print(" ")
+    
+def save_lexicons(y, dtf_features, p_value_limit, total_or_annotated):
+    #save lexicons
+    #reformat: each column is a list of words for the category
+    data_dict = {cat:[] for cat in y}
+    for cat in y:
+        cat_words = dtf_features[dtf_features["y"]==cat]
+        data_dict[cat] = cat_words["feature"].tolist()
+    # make lists of equal length
+    max_list_len = max([len(data_dict[cat]) for cat in data_dict])
+    for cat in data_dict:
+        if len(data_dict[cat]) < max_list_len:
+            for i in range(len(data_dict[cat]), max_list_len):
+                data_dict[cat].append("")
+    #save all words per category
+    lexicon = pd.DataFrame(data_dict)
+    lexicon.to_csv("results/Risk_Factor_Lexicons_"+total_or_annotated+"_"+str(np.round(1-p_value_limit,2))+".csv")    
 
 #formating annotations and text
 annotated_texts = []
 annotated_risk_factors = []
+non_annotated_texts = []
+non_annotated_labels = []
 for i in range(len(df)):
     text = df.at[i, 'text']
     labels = df.at[i, 'label']
+    prev_label_loc = 0
     for l in labels:
+        non_annotated_text = text[prev_label_loc:l[0]]
+        if len(non_annotated_text) > 1:
+            non_annotated_texts.append(non_annotated_text)
+            non_annotated_labels.append("None")
+        prev_label_loc = l[1]+1 #sets new start of nonannotated text
         annotated_text = text[l[0]:l[1]]
         annotated_risk_factor = l[2]
         annotated_texts.append(annotated_text)
         annotated_risk_factors.append(annotated_risk_factor)
-X = annotated_texts
+    non_annotated_text = text[prev_label_loc:-1] #gets the remainder of unlabeled text
+    non_annotated_texts.append(non_annotated_text)
+    non_annotated_labels.append("None")
 
+X_annotated_only = annotated_texts #for just annotated text - i.e. words unique between risk factors
+X_total_text = annotated_texts + non_annotated_texts #for total text
 #formatting labels into one-hot encoding
-y_raw = annotated_risk_factors
-y = pd.DataFrame(np.zeros((len(y_raw), len(set(y_raw)))), columns=set(y_raw)).sort_index(axis=1)
-for i in range(len(y_raw)):
-    y.at[i, y_raw[i]] = 1
-
+y_annotated_raw = annotated_risk_factors
+y_annotated_only = format_y_labels(y_annotated_raw)
+#for total text
+y_total_raw = y_annotated_raw + non_annotated_labels
+y_total = format_y_labels(y_total_raw)
 #vectorize text
-vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1,3), norm='l2', max_features=10000)
-vectorizer.fit(X)
-X_vec = vectorizer.transform(X)
-
+X_annotated_only_vec, vectorizer_annotated_only = vectorize_text(X_annotated_only)
+#for total text
+X_total_vec, vectorizer_total = vectorize_text(X_total_text)
 #chi-squared for significant words
 #i.e., find words with statistically significiant difference in frequency between categories
-X_names = vectorizer.get_feature_names_out()
-p_value_limit = 0.99
-dtf_features = pd.DataFrame()
-for cat in y.columns:
-    chi2, p = feature_selection.chi2(X_vec, y[[cat]])
-    dtf_features = dtf_features.append(pd.DataFrame(
-                   {"feature":X_names, "score":1-p, "y":cat}))
-    dtf_features = dtf_features.sort_values(["y","score"], 
-                    ascending=[True,False])
-    dtf_features = dtf_features[dtf_features["score"]>p_value_limit]
-X_names = dtf_features["feature"].unique().tolist()
-
-#print top words
-for cat in y:
-    print("# {}:".format(cat))
-    print("  . selected features:",
-          len(dtf_features[dtf_features["y"]==cat]))
-    print("  . top features:", ",".join(dtf_features[dtf_features["y"]==cat]["feature"].values[:10]))
-    print(" ")
-    
+p_val = 0.99 #Change P-val here
+dtf_features_annotated_only = get_significant_words(vectorizer_annotated_only, X_annotated_only_vec, y_annotated_only, p_value_limit=p_val)
+dtf_features_total = get_significant_words(vectorizer_total, X_total_vec, y_total, p_value_limit=p_val)
+#print results
+print("Using only annotated text: \n")
+print_results(y_annotated_only, dtf_features_annotated_only)
+print("Using total text: \n")
+print_results(y_total, dtf_features_total)
 #save lexicons
-#reformat: each column is a list of words for the category
-data_dict = {cat:[] for cat in y}
-for cat in y:
-    cat_words = dtf_features[dtf_features["y"]==cat]
-    data_dict[cat] = cat_words["feature"].tolist()
-# make lists of equal length
-max_list_len = max([len(data_dict[cat]) for cat in data_dict])
-for cat in data_dict:
-    if len(data_dict[cat]) < max_list_len:
-        for i in range(len(data_dict[cat]), max_list_len):
-            data_dict[cat].append("")
-#save all words per category
-lexicon = pd.DataFrame(data_dict)
-lexicon.to_csv("results/Risk_Factor_Lexicons.csv")    
+save_lexicons(y_annotated_only, dtf_features_annotated_only, p_val, total_or_annotated="annotated_only")
+save_lexicons(y_total, dtf_features_total, p_val, total_or_annotated="total")
--- risk_factor_score_script.py
+++ risk_factor_score_script.py
@@ -1,14 +1,39 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jun 24 09:41:19 2022
-
+Update: removed topic model plus dependency
 @author: srandrad
 """
 
 import pandas as pd
 import sys
 import os
-from module.topic_model_plus_class import Topic_Model_plus
+from time import sleep
+from tqdm import tqdm
+
+def prepare_data(file, combine_cols, id_col, extra_cols=[]):
+    # load data
+    data_df = pd.read_csv(open(file,encoding='utf8',errors='ignore'))
+    #combine columns
+    columns_to_drop = [cols for cols in data_df.columns if cols not in [id_col]+extra_cols]
+    rows_to_drop = []
+    combined_text = []
+    sleep(0.5)
+    for i in tqdm(range(0, len(data_df)), "Combining Columns…"):
+        text = ""
+        for attr in combine_cols:
+            if not(str(data_df.iloc[i][attr]).strip("()").lower().startswith("see") or str(data_df.iloc[i][attr]).strip("()").lower().startswith("same") or str(data_df.iloc[i][attr])=="" or isinstance(data_df.iloc[i][attr],float) or str(data_df.iloc[i][attr]).lower().startswith("none")):
+                if text != "":
+                    text += ". " 
+                text += str(data_df.iloc[i][attr])
+        if text == "":
+            rows_to_drop.append(i)
+        combined_text.append(text)
+    sleep(0.5)
+    data_df["Combined Text"] = combined_text
+    data_df = data_df.drop(columns_to_drop, axis=1)
+    data_df = data_df.drop(rows_to_drop).reset_index(drop=True)
+    return data_df
 
 #read in lexicons
 lexicon_file = os.path.join('results', 'Risk_Factor_Lexicons.csv')
@@ -16,9 +41,8 @@ lexicons = pd.read_csv(lexicon_file, index_col=0)
 #read in llis using topic model plus
 llis_file = os.path.join('data', 'useable_LL_combined.csv')
 combine_cols = ['Abstract', 'Lesson(s) Learned', 'Recommendation(s)', 'Driving Event']
-llis = Topic_Model_plus(list_of_attributes=combine_cols, document_id_col='Lesson ID', csv_file=llis_file, combine_cols=True)
-llis.prepare_data(dtype=str) #used to combine the columns
-
+llis_df = prepare_data(llis_file, combine_cols, 'Lesson ID')
+#combining columns, loading data
 def calc_risk_factor_scores(df, text_col, lexicons):
     risk_factors = [c for c in lexicons.columns]
     risk_factor_words = {risk_factor: lexicons[risk_factor].dropna().tolist() for risk_factor in risk_factors}
@@ -43,6 +67,6 @@ def calc_risk_factor_scores(df, text_col, lexicons):
     df_with_scores = pd.concat([df, pd.DataFrame(risk_factor_scores)], axis=1)
     return df_with_scores
 
-llis_with_scores = calc_risk_factor_scores(llis.data_df, 'Combined Text', lexicons)
+llis_with_scores = calc_risk_factor_scores(llis_df, 'Combined Text', lexicons)
 print(llis_with_scores)
 #llis_with_scores.to_csv(os.path.join('results', 'llis_with_risk_factor_scores.csv'))
