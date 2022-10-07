1a773391c7594870acb8c81cea8090eb9aa20cbc
srandrade <sequoia.r.andrade@nasa.gov>
2022-10-07 15:31:58 -0700

    edited for training

--- dev/safeaerobert_train_script.py
+++ dev/safeaerobert_train_script.py
@@ -22,6 +22,7 @@ cuda.empty_cache()
 print(device)
 import os
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 #load training data: ASRS, NTSB
 ASRS_file = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)),'data/ASRS/ASRS_1988_2022.csv')
