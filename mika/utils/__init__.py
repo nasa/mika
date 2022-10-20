679b852279b1aa4f3cd6b4241f6fabeb415ec96a
hswalsh <hannah.s.walsh@nasa.gov>
2022-10-20 15:52:03 -0700

    updated init file: ICS is no longer commented out

--- mika/utils/__init__.py
+++ mika/utils/__init__.py
@@ -4,7 +4,7 @@ Created on Wed Aug 24 15:11:22 2022
 __init__ file to import all utils files, functions, and classes
 @author: srandrad
 """
-#from mika.utils import ICS
+from mika.utils import ICS
 from mika.utils import SAFECOM
 from mika.utils import LLIS
 from mika.utils import SAFENET
