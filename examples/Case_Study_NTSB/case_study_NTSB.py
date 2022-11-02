b67ef9bbc6c4ffc6e16b988555d78beea30493d5
Hannah Walsh <hannah.s.walsh@nasa.gov>
2022-11-02 09:42:07 -0700

    progress towards NER/FMEA portion of ntsb case study
    progress towards NER/FMEA portion of ntsb case study - cuda
    
    progress towards NER/FMEA portion of ntsb case study - FMEA
    
    progress towards NER/FMEA portion of ntsb case study - Text

--- examples/Case_Study_NTSB/case_study_NTSB.py
+++ examples/Case_Study_NTSB/case_study_NTSB.py
@@ -11,8 +11,9 @@ from mika.ir import search
 from datetime import datetime as dt
 from mika.kd.topic_model_plus import Topic_Model_plus
 from mika.kd import trend_analysis
-from mika.kd import NER
+from mika.kd import FMEA
 from sklearn.feature_extraction.text import CountVectorizer
+from torch import cuda
 
 # before loading data, grab only recent reports and save as a new csv
 ntsb_filepath = os.path.join("data/NTSB/ntsb_full.csv")
@@ -25,7 +26,7 @@ ntsb_df.to_csv(ntsb_recent_filepath)
 
 # now load into Data()
 ntsb_data = Data()
-ntsb_text_columns = ['narr_accf'] # narrative accident final and narrative accident cause
+ntsb_text_columns = ['narr_cause', 'narr_accf'] # narrative accident cause and narrative accident final
 ntsb_document_id_col = 'ev_id'
 ntsb_database_name = 'NTSB'
 ntsb_data.load(ntsb_recent_filepath, preprocessed=False, text_columns=ntsb_text_columns, id_col=ntsb_document_id_col, name=ntsb_database_name,preprocessed_kwargs={'dtype':str}) # way to load as str?
@@ -38,12 +39,12 @@ model = os.path.join('models', 'fine_tuned_llis_model')
 query = 'fatigue crack'
 ir_ntsb = search(ntsb_data, model)
 embeddings_path = os.path.join('data', 'LLIS', 'llis_sentence_embeddings_finetune.npy')
-embeddings_path = os.path.join('data', 'LLIS', 'llis_sentence_embeddings.npy')
-ir_ntsb.get_sentence_embeddings(embeddings_path) # comment this out if the embeddings already exist
-#ir_ntsb.load_sentence_embeddings(embeddings_path) # uncomment this if you wish to load sentence embeddings that already exist
+#embeddings_path = os.path.join('data', 'LLIS', 'llis_sentence_embeddings.npy')
+#ir_ntsb.get_sentence_embeddings(embeddings_path) # comment this out if the embeddings already exist
+ir_ntsb.load_sentence_embeddings(embeddings_path) # uncomment this if you wish to load sentence embeddings that already exist
 print(ir_ntsb.run_search(query,return_k=5))
 
-# taxonomy
+# bert topics
 # - narr_cause and narr_accf/narr_accp - these are in ntsb_full_narratives.csv
 tm = Topic_Model_plus(text_columns=ntsb_text_columns, data=ntsb_data)
 vectorizer_model = CountVectorizer(ngram_range=(1, 3), stop_words="english") #removes stopwords
@@ -53,10 +54,25 @@ tm.save_bert_model()
 BERTkwargs={"calculate_probabilities":True, "top_n_words": 20, 'min_topic_size':150}
 tm.bert_topic(count_vectorizor=vectorizer_model, BERTkwargs=BERTkwargs, from_probs=True)
 tm.save_bert_results(from_probs=True)
+tm.save_bert_taxonomy()
 
 # NER for FMEA
-# - rows: event table
+# - rows: narratives table
 # - severities: injuries table
-#
+model_checkpoint = os.path.join("models", "FMEA-ner-model", "checkpoint-1424")
+print(model_checkpoint)
+
+device = 'cuda' if cuda.is_available() else 'cpu'
+cuda.empty_cache()
+print(device)
+
+fmea = FMEA()
+fmea.load_model(model_checkpoint)
+print("loaded model")
+input_data = fmea.load_data(ntsb_recent_filepath, formatted=False, text_col='narr_accf')
+
+print("loaded data")
+preds = fmea.predict()
+
 # HEAT
 # - aircraft info table
