7fb9004a3362185ae96940dcade9083998add11d
Hannah Walsh <hannah.s.walsh@nasa.gov>
2023-03-16 13:07:00 -0700

    added new queries

--- examples/fishbone_example/fishbone_script.py
+++ examples/fishbone_example/fishbone_script.py
@@ -29,15 +29,15 @@ asrs_data.load(asrs_filepath_edit, preprocessed=False, text_columns=asrs_text_co
 asrs_data.prepare_data(create_ids=True, combine_columns=asrs_text_columns, remove_incomplete_rows=False)
 
 # bert topics - might be good for identifying general categories to use as branches
-# tm = Topic_Model_plus(text_columns=asrs_text_columns, data=asrs_data)
-# vectorizer_model = CountVectorizer(ngram_range=(1, 3), stop_words="english") #removes stopwords
-# tm.bert_topic(sentence_transformer_model=None, umap=None, hdbscan=None, count_vectorizor=vectorizer_model, ngram_range=(1,3), BERTkwargs={}, from_probs=False, thresh=0.01)
-# tm.save_bert_model()
+tm = Topic_Model_plus(text_columns=asrs_text_columns, data=asrs_data)
+vectorizer_model = CountVectorizer(ngram_range=(1, 3), stop_words="english") #removes stopwords
+tm.bert_topic(sentence_transformer_model=None, umap=None, hdbscan=None, count_vectorizor=vectorizer_model, ngram_range=(1,3), BERTkwargs={}, from_probs=False, thresh=0.01)
+tm.save_bert_model()
 
-# BERTkwargs={"top_n_words": 10, 'min_topic_size':5}
-# tm.bert_topic(count_vectorizor=vectorizer_model, BERTkwargs=BERTkwargs, from_probs=True)
-# tm.save_bert_results(from_probs=True)
-# tm.save_bert_taxonomy()
+BERTkwargs={"top_n_words": 10, 'min_topic_size':5}
+tm.bert_topic(count_vectorizor=vectorizer_model, BERTkwargs=BERTkwargs, from_probs=True)
+tm.save_bert_results(from_probs=True)
+tm.save_bert_taxonomy()
 
 # IR - might be good for filling out branches once you have an idea of general categories
 model = SentenceTransformer('all-distilroberta-v1')
@@ -63,7 +63,11 @@ queries = [
     'do drones fly above 400 feet above ground level',
     'do drones fly in restricted areas',
     'do drones collide with terrain',
-    'do drones fly without waivers when a waiver is required'
+    'do drones fly without waivers when a waiver is required',
+    'do drones have difficulties landing on certain surfaces',
+    'have drones collided with buildings',
+    'have drones collided with trees',
+    'have drones collided with objects'
     ]
 search_result = pd.DataFrame()
 for query in queries:
