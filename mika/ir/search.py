4568f6703ae1818811a404f2ceff1183dc51c0fa
Hannah Walsh <hannah.s.walsh@nasa.gov>
2023-03-30 17:00:54 -0700

    added passage function and removed id output option to reduce confusion about doc id outputs

--- mika/ir/search.py
+++ mika/ir/search.py
@@ -13,20 +13,20 @@ class search():
     ----------
     data : Data() object
         Data object loaded using mika.utils.Data
+    column_to_search : str
+        name of column to search - use "Combined Text" if it is desired to search all text columns (must first be defined in Data object)
     model :
         
     """
 
-    def __init__(self, data=None, model=''):
-        self.cols = data.text_columns
+    def __init__(self, column_to_search, data=None, model=''):
         self.sbert_model = model
         self.doc_ids = data.data_df[data.id_col]
         
-        self.corpus = [data.data_df[col].tolist() for col in self.cols]
-        self.corpus = [item for sublist in self.corpus for item in sublist]
-        self.corpus_ids = [self.doc_ids.tolist() for col in self.cols]
-        self.corpus_ids = [item for sublist in self.corpus_ids for item in sublist]
+        self.corpus = data.data_df[column_to_search].tolist()
+        self.corpus_ids = self.doc_ids.tolist()
         self.__make_sentence_corpus()
+        self.__make_passage_corpus()
         return
     
     def __make_sentence_corpus(self):
@@ -39,7 +39,21 @@ class search():
                 self.sentence_corpus.append(sentence)
                 self.sentence_doc_idx.append(i)
             i = i + 1
-    
+
+    def __make_passage_corpus(self):
+        self.passage_corpus = []
+        self.passage_doc_idx = []
+        passage_length = 3
+        i = 0
+        for doc in self.corpus:
+            sentences = sent_tokenize(doc)
+            num_sentences = len(sentences)
+            for start_idx in range(0, num_sentences, passage_length):
+                end_idx = min(start_idx+passage_length, num_sentences)
+                self.passage_corpus.append(" ".join(sentences[start_idx:end_idx]))
+                self.passage_doc_idx.append(i)
+            i = i + 1
+
     def get_sentence_embeddings(self, savepath):
         """
         Compute sentence embeddings for the corpus.
@@ -76,7 +90,7 @@ class search():
         embeddings_as_numpy = np.load(filepath)
         self.sentence_corpus_embeddings = torch.from_numpy(embeddings_as_numpy)
     
-    def run_search(self, query, return_k=1, show_id=True):
+    def run_search(self, query, return_k=1):
         """
         Run the search using a query and loaded corpus embeddings.
         
@@ -86,15 +100,15 @@ class search():
             Query to search
         return_k : int
             Number of results to return
-        show_id : Bool
-            If true, returns document ID (in addition to index) in top_hits
         
         RETURNS
         -------
         top_hits : DataFrame
-            Index, scores, and text of top k hits, structured in a DataFrame
+            Doc ID, scores, and text of top k hits, structured in a DataFrame
         """
         
+        # semantic search
+
         k_range = range(0,return_k)
         query_embedding = self.sbert_model.encode(query, convert_to_tensor=True)
         hits = util.semantic_search(query_embedding, self.sentence_corpus_embeddings, top_k=return_k)
@@ -102,10 +116,10 @@ class search():
         top_hit_idx = [self.sentence_doc_idx[id_k] for id_k in top_hit_ids]
         top_hit_scores = [hits[0][k]['score'] for k in k_range]
         top_hit_text = [self.corpus[self.sentence_doc_idx[id_k]] for id_k in top_hit_ids]
-        if show_id == True:
-            top_hit_doc = [self.corpus_ids[top_hit_idx[id_j]] for id_j in range(0,len(top_hit_idx))]
-            return_data = {'top_hit_idx': top_hit_idx, 'top_hit_doc': top_hit_doc, 'top_hit_scores': top_hit_scores, 'top_hit_text': top_hit_text}
-        else:
-            return_data = {'top_hit_idx': top_hit_idx, 'top_hit_scores': top_hit_scores, 'top_hit_text': top_hit_text}
+        top_hit_doc = [self.corpus_ids[top_hit_idx[id_j]] for id_j in range(0,len(top_hit_idx))]
+        return_data = {'top_hit_doc': top_hit_doc, 'top_hit_scores': top_hit_scores, 'top_hit_text': top_hit_text}
         top_hits = pd.DataFrame(return_data)
+
+        # reranker
+
         return top_hits
