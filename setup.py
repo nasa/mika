5d7d1d9051e07c678060b7b73368cdca79c950d2
Sequoia Andrade <sequoia.r.andrade@nasa.gov>
2023-11-09 16:46:30 -0800

    updated setup to use new mika release

--- setup.py
+++ setup.py
@@ -4,27 +4,29 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="mika",
-    version="1.0.0",
+    name="nasa-mika",
+    version="1.0.2",
     author="Hannah Walsh and Sequoia Andrade",
-    author_email="hannah.s.walsh@nasa.gov; sequoia.r.andrade@nasa.gov",
+    author_email="hannah.s.walsh@nasa.gov",
+    maintainer_email= "sequoia.r.andrade@nasa.gov",
+    maintainer= "sequoiarose",
     description="Manager for Intelligent Knowledge Access (MIKA)",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/nasa/mika.git",
-    download_url="",
+    download_url="https://github.com/nasa/mika/archive/refs/tags/v1.0.2.tar.gz",
+    project_urls = {"Documentation": "https://nasa.github.io/mika/"},
     keywords=["Natural Language Processing", "Knowledge Management", "Topic Modeling"],
     packages=find_packages(exclude=["test", "examples", "docs", "data"]),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: NOSA",
-        "Operating System :: Linux, Window, and Mac",
+        "License :: OSI Approved",
         'Development Status :: 3 - Alpha'
         ],
     python_requires='>=3.8',
     install_requires=[
         'BERTopic', 'datasets', 'gensim', 'matplotlib', 'nltk', 'numpy', 'octis', 'pandas',
-        'pathlib', 'pingouin', 'pkg_resources', 'pyLDAvis', 'regex', 'scikit-learn', 'scipy', 'seaborn',
+        'pathlib', 'pingouin', 'pyLDAvis', 'regex', 'scikit-learn', 'scipy', 'seaborn',
         'sentence-transformers', 'spacy', 'symspellpy', 'tomotopy', 'transformers', 'wordcloud'
         ]
 )
